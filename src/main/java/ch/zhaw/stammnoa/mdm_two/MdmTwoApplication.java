1778c0d5c08490b07d790c8add80d736cfe99d19
noahstammm <stammnoa@students.zhaw.ch>
2023-05-02 13:44:07 +0200

    init project with spring initializr

---
+++ src/main/java/ch/zhaw/stammnoa/mdm_two/MdmTwoApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.stammnoa.mdm_two;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdmTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdmTwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/zhaw/stammnoa/mdm_two/MdmTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.stammnoa.mdm_two;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdmTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
